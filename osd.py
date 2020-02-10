c60f5fae64ac0eeed077401710983939931d591a
bt- <benjaming.taylor@gmail.com>
2020-02-09 18:18:56 -0600

    Rewrote lookup function.

--- osd.py
+++ osd.py
@@ -1,10 +1,39 @@
 import warnings
 import nec_tables
 
-def lookup(lookup_value, lst):
-    for value in lst:
-        if lookup_value <= value:
-            return value
+
+def lookup(lookup_value, table, keys=True):
+    """
+    Perform lookup in NEC table using a numeric value.
+
+    Returns the next highest value when the `lookup_value` does not match a
+    value in the table.
+
+    Parameters
+    ----------
+    lookup_value : numeric
+        Value to compare against values within the passed table.
+    table : dictionary
+        The NEC table to perform the lookup against. Import tables from
+        nec_tables module.  Many tables are nested and require indexing into
+        before passing to this function.
+    keys : bool, default True
+        If true, compares the `lookup_value`against the `table` keys.
+        If false, compares the `lookup_value` against the `table` values.
+
+    """
+    if not keys:
+        table = {val: key for key, val in table.items()}
+    vals_to_compare = list(table.keys())
+
+    if lookup_value > vals_to_compare[-1]:
+        return warnings.warn('Lookup value is outside of the table.')
+
+    for val in vals_to_compare:
+        # print('i: {}, value: {}'.format(i, val))
+        if lookup_value <= val:
+            key = val
+            return table[key]
 
 def get_ambient_temp_derate(ambient_temp, wire_insulation_temp,
                             table_insulation_temp=30):
@@ -43,155 +72,156 @@ def get_ambient_temp_derate(ambient_temp, wire_insulation_temp,
     return(((wire_insulation_temp - ambient_temp) /
             (wire_insulation_temp - table_insulation_temp)) ** 0.5)
 
-class  Circuit(object):
-    """docstring for circuits.
-    parent class for dc and ac circuits
-    Attributes
-    ----------
-    name: string
-    start: string
-        originating equipment of the circuit
-    end: string
-        terminating equipment of the circuit
-    voltage: int
-        design voltage for circuit- 480, 277, 120, 12470
-    current: float
-    length: float
-        length of circuit in feet
-    parallel_sets: int
-        number of paralle sets of conductors
-    ccc_count: int
-        Current carrying conductors per conduit. Does not include neutral.
-        For the conditions of use derate calculations
-    height_above_roof: float
-        height above roof in inches
-    temp_high_amb: float
-        Degrees Fahrenheit
-        Recommended ASHRAE design 2 percent high
-        Passed by parent class in future?
-    cond_material: string
-        Conductor metal as "Cu" or "Al"
-    cond_insulation: string
-        Conductor insulation type
-    cond_size_base: string
-    egc_material: string
-        Equipment ground condcutor metal as "Cu" or "Al"
-    egc_size_base: string
-        Equipment ground condcutor size
-    neutral: int
-        Use 1 to include neutral in conduit size calculation.
-        Integer > 1 to add condcutors the same size as the currenty carrying
-    conduit_size_SF: float
-        Safety factor for conduit size calculation.
-        Default is 1.3
-    conduit_type: string
 
-    Internal Attributes
-    ___________________
-    ocpd
-    rooftop_adder
-    cond_size_base
-    egc_size_base
-    v_drop_volts
-    v_drop_percent
-    v_drop_in_range
-    conduit_size
-
-    """
-    def __init__(self, name=None, start=None, end=None,
-                 voltage=None, current=None, length=None, parallel_sets=1,
-                 ccc_count=3, height_above_roof=3.5, temp_high_amb=None,
-                 cond_metal=None, cond_insulation=None, cond_size=None,
-                 egc_metal='Cu', egc_size_base=None, neutral=1, conduit_size_SF=1.3,
-                 conduit_type='EMT'):
-        super(Circuit, self).__init__()
-        self.name = name
-        self.start = start
-        self.end = end
-        self.voltage = voltage
-        self.current = current
-        self.length = length
-        self.parallel_sets = parallel_sets
-        self.ccc_count = ccc_count
-        self.height_above_roof = height_above_roof
-        self.temp_high_amb = temp_high_amb
-        self.cond_metal = cond_metal
-        self.cond_insulation = cond_insulation
-        self.cond_size = cond_size
-        self.egc_metal = egc_metal
-        self.egc_size_base = egc_size_base
-        self.neutral = neutral
-        self.conduit_size_SF = conduit_size_SF
-        self.conduit_type = conduit_type
-
-        self.ocpd = lookup(self.current * 1.25, nec_tables.ocpd_sizes)
-
-    def _update_attribute(self, arg):
-        """
-        Set attribute and recalulate dependent values.
-        """
-        pass
-
-    def _get_amb_temp_plus_rooftop_adder(self):
-        """[Needs to be updated]
-        Table removed in NEC 2017 (now only a single value), valid for 2008, 2011, 2014
-        Temperatures in Fahrenheit
-        """
-        if self.height_above_roof == -1:
-            return self.temp_high_amb
-        elif self.height_above_roof <= 0.5:
-            return self.temp_high_amb + 60
-        elif self.height_above_roof <= 3.5:
-            return self.temp_high_amb + 40
-        elif self.height_above_roof <= 12:
-            return self.temp_high_amb + 30
-        elif self.height_above_roof <= 36:
-            return self.temp_high_amb + 25
-        elif self.height_above_roof > 36:
-            return self.temp_high_amb + 0
-
-    def get_amb_temp_derate(self):
-        """Write method to use the top level function."""
-        pass
-
-    def _cond_per_raceway_derate(self):
-        """[Needs to be updated]
-        NEC 310.15(B)(3)(a) Adjustment factors for more than three
-        current-carrying conductors in a raceway or cable
-        """
-        if self.ccc_count < 4:
-            return(1.0)
-        elif self.ccc_count <= 6:
-            return(0.8)
-        elif self.ccc_count <= 9:
-            return(0.7)
-        elif self.ccc_count <= 20:
-            return(0.5)
-        elif self.ccc_count <= 30:
-            return(0.45)
-        elif self.ccc_count <= 40:
-            return(0.4)
-        elif self.ccc_count >= 41:
-            return(0.35)
-
-
-class DcCircuit(Circuit):
-    """docstring for DcCircuit.
-    Class for AC circuits.
-
-    """
-    def __init__(self, name=None, ccc_count=2):
-        super(DcCircuit, self).__init__()
-        self.name = name
-        self.ccc_count = ccc_count
-
-class AcCircuit(Circuit):
-    """docstring for AcCircuit.
-    Class for AC circuits.
-    """
-    def __init__(self, arg):
-        super(AcCircuit, self).__init__()
-        self.arg = arg
-
-if __name__ == '__main__':
-    main()
+# class Circuit(object):
+#     """docstring for circuits.
+#     parent class for dc and ac circuits
+#     Attributes
+#     ----------
+#     name: string
+#     start: string
+#         originating equipment of the circuit
+#     end: string
+#         terminating equipment of the circuit
+#     voltage: int
+#         design voltage for circuit- 480, 277, 120, 12470
+#     current: float
+#     length: float
+#         length of circuit in feet
+#     parallel_sets: int
+#         number of paralle sets of conductors
+#     ccc_count: int
+#         Current carrying conductors per conduit. Does not include neutral.
+#         For the conditions of use derate calculations
+#     height_above_roof: float
+#         height above roof in inches
+#     temp_high_amb: float
+#         Degrees Fahrenheit
+#         Recommended ASHRAE design 2 percent high
+#         Passed by parent class in future?
+#     cond_material: string
+#         Conductor metal as "Cu" or "Al"
+#     cond_insulation: string
+#         Conductor insulation type
+#     cond_size_base: string
+#     egc_material: string
+#         Equipment ground condcutor metal as "Cu" or "Al"
+#     egc_size_base: string
+#         Equipment ground condcutor size
+#     neutral: int
+#         Use 1 to include neutral in conduit size calculation.
+#         Integer > 1 to add condcutors the same size as the currenty carrying
+#     conduit_size_SF: float
+#         Safety factor for conduit size calculation.
+#         Default is 1.3
+#     conduit_type: string
+#
+#     Internal Attributes
+#     ___________________
+#     ocpd
+#     rooftop_adder
+#     cond_size_base
+#     egc_size_base
+#     v_drop_volts
+#     v_drop_percent
+#     v_drop_in_range
+#     conduit_size
+#
+#     """
+#     def __init__(self, name=None, start=None, end=None,
+#                  voltage=None, current=None, length=None, parallel_sets=1,
+#                  ccc_count=3, height_above_roof=3.5, temp_high_amb=None,
+#                  cond_metal=None, cond_insulation=None, cond_size=None,
+#                  egc_metal='Cu', egc_size_base=None, neutral=1, conduit_size_SF=1.3,
+#                  conduit_type='EMT'):
+#         super(Circuit, self).__init__()
+#         self.name = name
+#         self.start = start
+#         self.end = end
+#         self.voltage = voltage
+#         self.current = current
+#         self.length = length
+#         self.parallel_sets = parallel_sets
+#         self.ccc_count = ccc_count
+#         self.height_above_roof = height_above_roof
+#         self.temp_high_amb = temp_high_amb
+#         self.cond_metal = cond_metal
+#         self.cond_insulation = cond_insulation
+#         self.cond_size = cond_size
+#         self.egc_metal = egc_metal
+#         self.egc_size_base = egc_size_base
+#         self.neutral = neutral
+#         self.conduit_size_SF = conduit_size_SF
+#         self.conduit_type = conduit_type
+#
+#         self.ocpd = lookup(self.current * 1.25, nec_tables.ocpd_sizes)
+#
+#     def _update_attribute(self, arg):
+#         """
+#         Set attribute and recalulate dependent values.
+#         """
+#         pass
+#
+#     def _get_amb_temp_plus_rooftop_adder(self):
+#         """[Needs to be updated]
+#         Table removed in NEC 2017 (now only a single value), valid for 2008, 2011, 2014
+#         Temperatures in Fahrenheit
+#         """
+#         if self.height_above_roof == -1:
+#             return self.temp_high_amb
+#         elif self.height_above_roof <= 0.5:
+#             return self.temp_high_amb + 60
+#         elif self.height_above_roof <= 3.5:
+#             return self.temp_high_amb + 40
+#         elif self.height_above_roof <= 12:
+#             return self.temp_high_amb + 30
+#         elif self.height_above_roof <= 36:
+#             return self.temp_high_amb + 25
+#         elif self.height_above_roof > 36:
+#             return self.temp_high_amb + 0
+#
+#     def get_amb_temp_derate(self):
+#         """Write method to use the top level function."""
+#         pass
+#
+#     def _cond_per_raceway_derate(self):
+#         """[Needs to be updated]
+#         NEC 310.15(B)(3)(a) Adjustment factors for more than three
+#         current-carrying conductors in a raceway or cable
+#         """
+#         if self.ccc_count < 4:
+#             return(1.0)
+#         elif self.ccc_count <= 6:
+#             return(0.8)
+#         elif self.ccc_count <= 9:
+#             return(0.7)
+#         elif self.ccc_count <= 20:
+#             return(0.5)
+#         elif self.ccc_count <= 30:
+#             return(0.45)
+#         elif self.ccc_count <= 40:
+#             return(0.4)
+#         elif self.ccc_count >= 41:
+#             return(0.35)
+#
+#
+# class DcCircuit(Circuit):
+#     """docstring for DcCircuit.
+#     Class for AC circuits.
+#
+#     """
+#     def __init__(self, name=None, ccc_count=2):
+#         super(DcCircuit, self).__init__()
+#         self.name = name
+#         self.ccc_count = ccc_count
+#
+# class AcCircuit(Circuit):
+#     """docstring for AcCircuit.
+#     Class for AC circuits.
+#     """
+#     def __init__(self, arg):
+#         super(AcCircuit, self).__init__()
+#         self.arg = arg
+#
+# if __name__ == '__main__':
+#     main()
--- test_osd.py
+++ test_osd.py
@@ -1,6 +1,35 @@
 import unittest
 import pytest
 import osd
+import nec_tables as nec
+
+
+class TestLookup:
+    """Test lookup function against various NEC tables."""
+
+    def test_warn_lookup_out_of_table(self):
+        """Raise warning if the lookup value is outside of the table."""
+        with pytest.warns(UserWarning):
+            osd.lookup(43, nec.ccc_count_derate)
+        with pytest.warns(UserWarning):
+            osd.lookup(800, nec.cable_ampacity_310_16['Cu'][90], keys=False)
+
+    def test_ccc_derate_table(self):
+        """Test current carrying conductor derate table."""
+        assert osd.lookup(3, nec.ccc_count_derate) == 1.0
+        assert osd.lookup(2, nec.ccc_count_derate) == 1.0
+        assert osd.lookup(4, nec.ccc_count_derate) == 0.8
+        assert osd.lookup(5, nec.ccc_count_derate) == 0.8
+        assert osd.lookup(10, nec.ccc_count_derate) == 0.5
+
+    def test_cable_ampacities(self):
+        """Test lookups against ampacities in Table 310.16."""
+        assert osd.lookup(135, nec.cable_ampacity_310_16['Cu'][90],
+                          keys=False) == '1'
+        assert osd.lookup(225, nec.cable_ampacity_310_16['Cu'][90],
+                          keys=False) == '3/0'
+        assert osd.lookup(25, nec.cable_ampacity_310_16['Cu'][90],
+                          keys=False) == '12'
 
 
 class TestGetAmbientTempDerate:
@@ -31,57 +60,57 @@ class TestGetAmbientTempDerate:
 
 
 
-ac_circ = osd.Circuit(name='inv01', start='INV.01', end='PV.PNLBD.01',
-                      voltage=480, current=28.9, length=165, parallel_sets=1,
-                      ccc_count=3, height_above_roof=3.5,
-                      temp_high_amb=97, cond_metal='Al', cond_insulation='THWN-2',
-                      cond_size=None, egc_metal='Cu', egc_size_base=None, neutral=1,
-                      conduit_size_SF=1.3, conduit_type='EMT')
-
-class OsdTests(unittest.TestCase):
-    """Tests for osd.py."""
-    def test_rooftop_adder(self):
-        roof_adders = [(0.5, 60), (0.501, 40), (3.5, 40), (3.501, 30), (12, 30),
-                       (12.01, 25), (36, 25), (36.01, 0)]
-        for height_temp_adder_pair in roof_adders:
-            print('########################')
-            print(height_temp_adder_pair)
-            ac_circ.height_above_roof = height_temp_adder_pair[0]
-            print('height above roof: {}'.format(ac_circ.height_above_roof))
-            temp = height_temp_adder_pair[1] + ac_circ.temp_high_amb
-            print('check temp: {}'.format(temp))
-            self.assertEqual(temp, ac_circ._get_amb_temp_plus_rooftop_adder(),
-                             'amb_temp: {}, height above roof: {},\
-                              amb + rooftop adder: {}'.format(
-                              ac_circ.temp_high_amb,
-                              ac_circ.height_above_roof,
-                              ac_circ._get_amb_temp_plus_rooftop_adder()))
-
-    def test_amb_temp_corr(self):
-        ac_circ.height_above_roof = 3
-        ac_circ.temp_high_amb = 97
-        self.assertEqual(round(ac_circ._amb_temp_correction(),5), 0.72648)
-
-        ac_circ.height_above_roof = 3
-        ac_circ.temp_high_amb = 95
-        self.assertEqual(round(ac_circ._amb_temp_correction(),5), 0.73912)
-
-        ac_circ.height_above_roof = 3
-        ac_circ.temp_high_amb = 88
-        self.assertEqual(round(ac_circ._amb_temp_correction(),5), 0.78174)
-
-        ac_circ.height_above_roof = 3
-        ac_circ.temp_high_amb = 101
-        self.assertEqual(round(ac_circ._amb_temp_correction(),5), 0.70053)
-
-    def test_cond_per_raceway_derate(self):
-        cond_derates = [(3,1),(4,0.8),(6,0.8),(7,0.7),(9,0.7),(10,0.5),
-                        (20,0.5),(21,0.45),(30,0.45),(31,0.4),(40,0.4),
-                        (41,0.35)]
-        for pair in cond_derates:
-            ac_circ.ccc_count = pair[0]
-            self.assertEqual(pair[1], ac_circ._cond_per_raceway_derate(),
-                             'Conductors per conduit derate test')
-
-if __name__ == '__main__':
-    unittest.main()
+# ac_circ = osd.Circuit(name='inv01', start='INV.01', end='PV.PNLBD.01',
+#                       voltage=480, current=28.9, length=165, parallel_sets=1,
+#                       ccc_count=3, height_above_roof=3.5,
+#                       temp_high_amb=97, cond_metal='Al', cond_insulation='THWN-2',
+#                       cond_size=None, egc_metal='Cu', egc_size_base=None, neutral=1,
+#                       conduit_size_SF=1.3, conduit_type='EMT')
+#
+# class OsdTests(unittest.TestCase):
+#     """Tests for osd.py."""
+#     def test_rooftop_adder(self):
+#         roof_adders = [(0.5, 60), (0.501, 40), (3.5, 40), (3.501, 30), (12, 30),
+#                        (12.01, 25), (36, 25), (36.01, 0)]
+#         for height_temp_adder_pair in roof_adders:
+#             print('########################')
+#             print(height_temp_adder_pair)
+#             ac_circ.height_above_roof = height_temp_adder_pair[0]
+#             print('height above roof: {}'.format(ac_circ.height_above_roof))
+#             temp = height_temp_adder_pair[1] + ac_circ.temp_high_amb
+#             print('check temp: {}'.format(temp))
+#             self.assertEqual(temp, ac_circ._get_amb_temp_plus_rooftop_adder(),
+#                              'amb_temp: {}, height above roof: {},\
+#                               amb + rooftop adder: {}'.format(
+#                               ac_circ.temp_high_amb,
+#                               ac_circ.height_above_roof,
+#                               ac_circ._get_amb_temp_plus_rooftop_adder()))
+#
+#     def test_amb_temp_corr(self):
+#         ac_circ.height_above_roof = 3
+#         ac_circ.temp_high_amb = 97
+#         self.assertEqual(round(ac_circ._amb_temp_correction(),5), 0.72648)
+#
+#         ac_circ.height_above_roof = 3
+#         ac_circ.temp_high_amb = 95
+#         self.assertEqual(round(ac_circ._amb_temp_correction(),5), 0.73912)
+#
+#         ac_circ.height_above_roof = 3
+#         ac_circ.temp_high_amb = 88
+#         self.assertEqual(round(ac_circ._amb_temp_correction(),5), 0.78174)
+#
+#         ac_circ.height_above_roof = 3
+#         ac_circ.temp_high_amb = 101
+#         self.assertEqual(round(ac_circ._amb_temp_correction(),5), 0.70053)
+#
+#     def test_cond_per_raceway_derate(self):
+#         cond_derates = [(3,1),(4,0.8),(6,0.8),(7,0.7),(9,0.7),(10,0.5),
+#                         (20,0.5),(21,0.45),(30,0.45),(31,0.4),(40,0.4),
+#                         (41,0.35)]
+#         for pair in cond_derates:
+#             ac_circ.ccc_count = pair[0]
+#             self.assertEqual(pair[1], ac_circ._cond_per_raceway_derate(),
+#                              'Conductors per conduit derate test')
+#
+# if __name__ == '__main__':
+#     unittest.main()
